e1028be630dc4e230166f3b0b1a3b991b8695293
我 <wo@wodeiMac.local>
2015-09-29 17:01:31 +0800

    Initial Commit

---
+++ gaode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gaode
+//
+//  Created by 我 on 15/9/29.
+//  Copyright (c) 2015年 我. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gaode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gaode
+//
+//  Created by 我 on 15/9/29.
+//  Copyright (c) 2015年 我. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
